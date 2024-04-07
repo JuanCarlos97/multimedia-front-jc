6e890a42687e136063d429d0672a641720fe7d72
Juan Carlos <juan.jc971@gmail.com>
2024-04-07 14:49:19 -0600

    firebase added and change title app

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://octopus-app-ia87y.ondigitalocean.app/api',
 });
 
 API.interceptors.request.use(
